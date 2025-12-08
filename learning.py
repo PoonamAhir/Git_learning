3db42fa4f0934aab9e592b0b04d06da357bf3789
Poonam <punamahir09@gmail.com>
2025-12-08 11:53:18 +0530

    adding range

--- learning.py
+++ learning.py
@@ -1,6 +1,5 @@
-num = int(input("Enter a number: "))
-
-if num%2 == 0:
-    print("Even number")
-else:
-    print("Odd number")
+for num in range(10,20):
+    if num%2 == 0:
+        print(f"{num} is Even number")
+    else:
+        print(f"{num} is Odd number")
